be0e0023de7cf5559b24c780f026e53a37bb412f
zubairhmd <zubairhmd84@gmail.com>
2021-08-24 12:17:58 +0600

    more map coding

---
+++ 5-more.map.js
@@ -0,0 +1,23 @@
+const friends = ['Tom Hanks', 'Tom Cruise', 'Tom Brady', 'Tom Solaiman'];
+
+const fLength = friends.map(friend => friend.length);
+// console.log(fLength);
+
+
+const products = [
+    { name: 'water bottle', price: 50, color: 'yellow' },
+    { name: 'mobile phone', price: 15000, color: 'black' },
+    { name: 'smart watch', price: 3000, color: 'black' },
+    { name: 'sticky note', price: 30, color: 'pink' },
+    { name: 'water glass', price: 3, color: 'white' }
+];
+
+const productNames = products.map(product => product.name);
+// console.log(productNames);
+const productPrice = products.map(product => product.price);
+// console.log(productPrice);
+const productColor = products.map(product => product.color)
+// console.log(productColor);
+
+// products.map(products => console.log(products));     // Return korbe
+products.forEach(products => console.log(products));    //Return korbe na
